35cfc211c1d10c4e0ec9dab9f15e4650738ba553
PadhyaRushi <110962038+PadhyaRushi@users.noreply.github.com>
2022-10-03 12:34:33 +0530

    Update classpr.cpp

--- classpr.cpp
+++ classpr.cpp
@@ -7,30 +7,26 @@ class parents
      	string bankname;    
 		string IFSC;
 		int Ac;
-		
-		 public:
+		public:
 	    	
 		void set()
 		{
 			cin>>bankname>>IFSC>>Ac;
-//			cout<<"BankName = "<<bankname<<"\nIFSC = "<<IFSC<<"\nAccount Number = "<<Ac<<endl;
+			cout<<"BankName = "<<bankname<<"\nIFSC = "<<IFSC<<"\nAccount Number = "<<Ac<<endl;
 		}
+		 
 			
 		
 };
 
 class child : public parents
 {
-	public:
-			void set1()
-		{
-	   cout<<"BankName = "<<bankname<<"\nIFSC = "<<IFSC<<"\nAccount Number = "<<Ac<<endl;
-	}
+	
 };
 int main()
 {
   child p;
-   p.set1();
+   p.set();
 	return 0;
 	
 }
